2b9a522b88ce0eecf990b01663fa1c1c79ff8381
datsfilipe <datsfilipe.foss@proton.me>
2023-06-09 16:03:14 -0300

    feat: add slider component with fullscreen feature

---
+++ src/components/Slider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import SliderItem from './SliderItem.vue';
+import posts from '../utils/posts.json';
+import { ref } from 'vue'
+
+const current = ref(0)
+const increment = () => current.value++
+const decrement = () => current.value--
+</script>
+
+<template>
+  <div class="slider">
+    <button @click="decrement" :disabled="current === 0">Prev</button>
+    <button class="next" @click="increment" :disabled="current === posts.length - 1">Next</button>
+    <SliderItem :key="current" :title="posts[current].title" :img="posts[current].image" />
+  </div>
+</template>
