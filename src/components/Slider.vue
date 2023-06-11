1e540f60741338ebaf92f004c07c7591851e4eb8
datsfilipe <datsfilipe.foss@proton.me>
2023-06-11 13:33:43 -0300

    feat: add posts original links

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -12,6 +12,6 @@ const decrement = () => current.value--
   <div class="slider">
     <button @click="decrement" :disabled="current === 0">Prev</button>
     <button class="next" @click="increment" :disabled="current === posts.length - 1">Next</button>
-    <SliderItem :key="current" :title="posts[current].title" :img="posts[current].image" />
+    <SliderItem :key="current" :title="posts[current].title" :img="posts[current].image" :link="posts[current].link"/>
   </div>
 </template>
--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-defineProps<{ title: string, img: string }>()
+defineProps<{ title: string, img: string, link?: string }>()
 
 const fullScreen = ref(false);
 const toggle = () => fullScreen.value = !fullScreen.value;
@@ -12,6 +12,11 @@ const toggle = () => fullScreen.value = !fullScreen.value;
     <div v-if="!fullScreen">
       <h1 class="title">{{ title }}</h1>
       <img :src="img" :alt="title" @click="toggle"/>
+      <a v-if="link" :href="link" target="_blank" class="link">
+        <i class="external-link-alt">
+          External link to original post.
+        </i>
+      </a>
     </div>
     <div v-else class="full-screen">
       <div class="full-screen-bg" @click="toggle"/>
